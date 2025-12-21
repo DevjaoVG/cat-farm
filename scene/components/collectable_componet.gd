992b4749b8f32c646a25a5bbd0f7ad3da7b51e9a
DevJao <joaovitorcontato1247@gmail.com>
2025-12-21 19:05:46 -0300

    feat: adiciona CollectableComponent ao projeto

---
+++ scene/components/collectable_componet.gd
@@ -0,0 +1,10 @@
+class_name CollectableComponent # Componente Reutilizavel
+extends Area2D
+
+@export var collectable_name: String # Nome do coletavel
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body == Player:
+		print("Coletou: ", collectable_name)
+		get_parent().queue_free()
